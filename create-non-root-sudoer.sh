1ddcba08a1d8c1e220e4c1dcb3fd3a28e7b033e9
Rahul Soibam <rahulsoibam@gmail.com>
2018-11-02 03:12:46 +0530

    Fix: Minor issues

--- create-non-root-sudoer.sh
+++ create-non-root-sudoer.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
+source ./lib/bot.sh
 
-read -r -p "Enter the username" username
+read -r -p "Enter the username: " username
 
 adduser $username
 
 usermod -aG sudo $username
 
-read -r -p "Make sudo user passwordless? [y|N]" response
+read -r -p "Make sudo user passwordless? [y|N]: " response
 
 if [[ $response =~ (yes|y|Y) ]];then
     sudo cp /etc/sudoers /etc/sudoers.bak
-    echo "$(username) ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
-    bot "User $(username) can now run sudo commands without password"
+    echo "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
+    bot "User $username can now run sudo commands without password"
 fi
 
 bot "Exit this session and login using the new user"
